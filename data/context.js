e13a22e888f3accf7757f0da41461a497cd5e64c
Diana <97.diana@gmail.com>
2015-04-01 20:42:03 +0300

    Loading some data

---
+++ data/context.js
@@ -0,0 +1,27 @@
+var context = [
+	{
+		"name": "Pașoptismul",
+		"lider": "Mihail Kogălniceanu",
+		"details": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit amet viverra neque. Vivamus vel facilisis tortor, ut posuere arcu. Mauris dignissim dapibus ligula, et faucibus elit condimentum at."
+	},
+	{
+		"name": "Junimea",
+		"lider": "Titu Maiorescu",
+		"details": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit amet viverra neque. Vivamus vel facilisis tortor, ut posuere arcu. Mauris dignissim dapibus ligula, et faucibus elit condimentum at."
+	},
+	{
+		"name": "Posteminescianism",
+		"lider": "-",
+		"details": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit amet viverra neque. Vivamus vel facilisis tortor, ut posuere arcu. Mauris dignissim dapibus ligula, et faucibus elit condimentum at."
+	},
+	{
+		"name": "Perioada interbelică",
+		"lider": "Eugen Lovinescu",
+		"details": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit amet viverra neque. Vivamus vel facilisis tortor, ut posuere arcu. Mauris dignissim dapibus ligula, et faucibus elit condimentum at."
+	},
+	{
+		"name": "Perioada postbelică",
+		"lider": "-",
+		"details": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit amet viverra neque. Vivamus vel facilisis tortor, ut posuere arcu. Mauris dignissim dapibus ligula, et faucibus elit condimentum at."
+	}
+]
--- js/pagechange.js
+++ js/pagechange.js
@@ -2,4 +2,17 @@ var app = angular.module('app', []);
 
 app.controller('PageChangeCtrl', function ($scope) {
 	$scope.selected = 1;
+	$scope.currentContext = context[0];
+	$scope.Contexts = 5;
+
+	$scope.hasLider = function() {
+		return $scope.currentContext.lider != "-";
+	}
+
+	$scope.changePage = function(x) {
+		if (x <= $scope.Contexts) {
+			$scope.selected = x;
+			$scope.currentContext = context[x - 1];
+		}
+	}
 });
