6662067139abb9565ab108b7c78994ebc3d6dd13
Diana <97.diana@gmail.com>
2015-04-18 20:23:32 +0300

    Building game

--- js/joc.js
+++ js/joc.js
@@ -1,4 +1,4 @@
-app.controller('JocCtrl', function ($scope) {
+app.controller('JocCtrl', function ($scope, $http) {
 	$scope.heartImg = [
 		"img/joc/heart_empty.png",
 		"img/joc/heart_25.png",
@@ -16,4 +16,42 @@ app.controller('JocCtrl', function ($scope) {
 		if ($scope.LifeLevel[$scope.CurrentLife] == 0) $scope.CurrentLife--;
 		if ($scope.CurrentLife == -1) $scope.Lost = true;
 	}
+
+	$scope.selectedQuestion = 0;
+	$scope.questionsNumber = 0;
+	$scope.questions;
+	$scope.checked = false;
+	$scope.correctAnswer = false;
+	$scope.asked = new Array();
+
+	$scope.check = function() {
+		$scope.checked = true;
+		if ($scope.GivenAnswer == $scope.questions[$scope.selectedQuestion].raspunsuri[$scope.questions[$scope.selectedQuestion].corect])
+			$scope.correctAnswer = true;
+		else $scope.correctAnswer = false;
+		showElement("QuestionRez");
+	}
+
+	$scope.chooseNextQuestion = function () {
+		$scope.selectedQuestion = Math.floor(Math.random() * $scope.questionsNumber);
+		while ($scope.asked[$scope.selectedQuestion]) $scope.selectedQuestion = Math.floor(Math.random() * $scope.questionsNumber);
+		$scope.asked[$scope.selectedQuestion] = true;
+		console.log($scope.selectedQuestion);
+	}
+
+	$scope.next = function() {
+		hideElement("QuestionRez")
+		$scope.checked = false;
+ 		$scope.chooseNextQuestion();
+	}
+
+	$http({method: 'GET', url: 'data/intrebari.json'}).success(function(data, status, headers, config) {
+		$scope.questions = data;
+		$scope.questionsNumber = $scope.questions.length;
+		for (var i = 0; i < $scope.questionsNumber; i++) {
+			$scope.asked[i] = false;
+		}
+ 		$scope.chooseNextQuestion();
+	});
+
 });
