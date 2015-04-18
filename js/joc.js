5000bef8e9d2db1b33d103efd3e3699545a84f20
Diana <97.diana@gmail.com>
2015-04-18 18:23:52 +0300

    Bug fixed

--- js/joc.js
+++ js/joc.js
@@ -6,14 +6,14 @@ app.controller('JocCtrl', function ($scope) {
 		"img/joc/heart_75.png",
 		"img/joc/heart_full.png"
 	]
-	$scope.LifeLevel = [4, 4, 4, 4, 4];
+	$scope.LifeLevel = [4, 4, 4];
 
-	$scope.CurrentLife = 4;
+	$scope.CurrentLife = 2;
 	$scope.Lost = false;
 
 	$scope.LoseLife = function() {
 		$scope.LifeLevel[$scope.CurrentLife]--;
 		if ($scope.LifeLevel[$scope.CurrentLife] == 0) $scope.CurrentLife--;
-		if ($scope.CurrentLife == -1) Lost = true;
+		if ($scope.CurrentLife == -1) $scope.Lost = true;
 	}
 });
