faaaf48dedbc3829877a15235e6f2e02738516ce
Diana <97.diana@gmail.com>
2015-04-23 17:09:57 +0300

    Building Teme Maker

---
+++ js/tememaker.js
@@ -0,0 +1,56 @@
+app.controller('TemeMakerCtrl', function ($scope) {
+	$scope.Theme;
+	$scope.Hypothesis;
+	$scope.Arguments = new Array();
+	$scope.pros = 0;
+	$scope.cons = 0;
+
+	$scope.Arguments[0] = {
+		"domain": "",
+		"text": "",
+		"type": "pro",
+		"okay": true,
+		"hasDomain": true
+	}
+	$scope.Arguments[1] = {
+		"domain": "",
+		"text": "",
+		"type": "pro",
+		"okay": true,
+		"hasDomain": true
+	}
+
+	$scope.addArgument = function() {
+		$scope.Arguments.push({
+			"domain": "",
+			"text": "",
+			"type": "pro",
+			"okay": true,
+		"hasDomain": true
+		});
+	}
+
+	$scope.checkText = function(s) {
+		var l = s.length();
+		s = s.toLowerCase();
+		var letters = 0;
+		for (var i = 0; i < l; i++)
+			if ('a' <= s && s <= 'z') letters++;
+		console.log(letters);
+		if (letters <= 3) return false;
+	}
+
+	$scope.verifyArguments = function() {
+		var l = $scope.Arguments.length();
+
+		for (var i = 0; i < l; i++) {
+			if ($scope.checkText($scope.Arguments[i].domain) == false) $scope.Arguments[i].hasDomain = false;
+			if ($scope.checkText($scope.Arguments[i].text) == false) $scope.Arguments[i].okay = false;
+			else {
+				if ($scope.Arguments[i].type == "pro") $scope.pros++;
+				else $scope.cons++;
+			}
+		}
+		console.log($scope.pros, $scope.cons);
+	}
+});
