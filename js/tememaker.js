a4178bb89f9f645e3fe33849d3f442a953208b00
Diana <97.diana@gmail.com>
2015-04-23 18:54:08 +0300

    Teme Maker Works

--- js/tememaker.js
+++ js/tememaker.js
@@ -2,12 +2,15 @@ app.controller('TemeMakerCtrl', function ($scope) {
 	$scope.Theme = "";
 	$scope.Hypothesis = "";
 	$scope.Arguments = new Array();
+	$scope.Conclusion = "";
+
 	$scope.pros = 0;
 	$scope.cons = 0;
 	$scope.verified = false;
-	$scope.result;
+	$scope.results = new Array;
 	$scope.hasErrors = false;
 	$scope.errors = "";
+	$scope.th = ["Primul ", "Al doilea ", "Al treilea ", "Al patrulea ", "Al cincilea ", "Al șaselea ", "Al șaptelea ", "Al optâlea", "Al nouălea", "Al zecelea"];
 
 	$scope.Arguments[0] = {
 		"domain": "",
@@ -40,21 +43,75 @@ app.controller('TemeMakerCtrl', function ($scope) {
 		var letters = 0;
 		for (var i = 0; i < l; i++) {
 			var code = s.charCodeAt(i);
-			console.log(code);
 			if ((code >= 97) && (code <= 122)) letters++;
 		}
-		console.log(s);
 		if (letters <= 3) return false;
 		return true;
+	}
+	
+
+	$scope.hasConjunction = function(s) {
+		var l = s.length;
+		//if (l < 2) return false;
+		if (s[0] == "c" || s[0] == "s") {
+			if (s[1] == "ă" || s[1] == "a") {
+				if (s[2] == " ") return true;
+				return false;
+			}
+			return false;
+		}
+
+		if (s[0] == "d" && s[1] == "a" && s[2] == "c" && (s[3] == "a" || s[3] == "ă") && s[4] == " ") return true;
+		return false;
 	}
 
 
 	$scope.generateEssay = function() {
-		console.log("generating...");
-
+		$scope.results = [];
 		$scope.result = "Pornind de la premisa că " + $scope.Hypothesis + ", voi demonstra " + $scope.Theme + ". ";
-		$scope.result = $scope.result + "/n bla bla";
+		$scope.results.push($scope.result);
+		$scope.crtpro = -1;
+		$scope.crtcon = -1;
+		var l = $scope.Arguments.length;
 
+		for (var i = 0; i < l; i++) {
+			$scope.result = "";
+			if ($scope.Arguments[i].okay) {
+				if ($scope.Arguments[i].type == "pro") {
+					$scope.crtpro++;
+					$scope.result += $scope.th[$scope.crtpro] + "argument ";
+					$scope.result += "în favoarea ipotezei ";
+				}
+				else {
+					$scope.crtcon++;
+					console.log("aici", $scope.crtcon);
+					if ($scope.crtcon == 0) $scope.result += "Pe de alta parte, un argument ";
+					else $scope.result += $scope.th[$scope.crtcon] + "argument ";
+					$scope.result += "în defavoarea ipotezei ";
+				}
+
+				if ($scope.Arguments[i].hasDomain) {
+					$scope.result += "provine din domeniul " + $scope.Arguments[i].domain + " și ";
+				}
+
+				if ($scope.hasConjunction($scope.Arguments[i].text)) $scope.result += "este ";
+				else $scope.result += "se referă la faptul că ";
+				$scope.result += $scope.Arguments[i].text + ".";
+				$scope.results.push($scope.result);
+			}
+		}
+
+		$scope.result = "În concluzie, " + $scope.Conclusion + ". ";
+		$scope.results.push($scope.result);
+	}
+
+	$scope.eliminateExtraPoints = function(s) {
+		var l = s.length;
+		l--;
+		while(l > 0 && s[l] == ".") l--;
+		l++;
+		s = s.substring(0, l);
+		return s;
 	}
 
 	$scope.enoughArguments = function() {
@@ -65,12 +122,17 @@ app.controller('TemeMakerCtrl', function ($scope) {
 		return ($scope.cons + $scope.pros < 2);
 	}
 
-	$scope.verifyArguments = function() {
+	$scope.verifyData = function() {
 		var l = $scope.Arguments.length;
 		$scope.pros = 0;
 		$scope.cons = 0;
 
+		$scope.Hypothesis = $scope.eliminateExtraPoints($scope.Hypothesis);
+		$scope.Theme = $scope.eliminateExtraPoints($scope.Theme);
+
 		for (var i = 0; i < l; i++) {
+			$scope.Arguments[i].domain = $scope.eliminateExtraPoints($scope.Arguments[i].domain);
+			$scope.Arguments[i].text = $scope.eliminateExtraPoints($scope.Arguments[i].text);
 			if ($scope.checkText($scope.Arguments[i].domain) == false) {
 				$scope.Arguments[i].hasDomain = false;
 				$scope.Arguments[i].domain = "";
@@ -85,8 +147,8 @@ app.controller('TemeMakerCtrl', function ($scope) {
 				else $scope.cons++;
 			}
 		}
+		$scope.Conclusion = $scope.eliminateExtraPoints($scope.Conclusion);
 
-		$scope.verified = true;
 
 		if ($scope.notEnoughArguments()) {
 			$scope.hasErrors = true;
@@ -100,8 +162,13 @@ app.controller('TemeMakerCtrl', function ($scope) {
 			$scope.hasErrors = true;
 			$scope.errors += "Nu ai completat corect tema premisei. ";
 		}
+		if ($scope.checkText($scope.Conclusion) == false) {
+			$scope.hasErrors = true;
+			$scope.errors += "Nu ai completat concluzia. ";
+		}
 
 
+		$scope.verified = true;
 		if ($scope.enoughArguments()) $scope.generateEssay();
 	}
 
