e26022f9ee4c07e33c51283ea0f8862eb27b6fc0
Diana <97.diana@gmail.com>
2015-07-23 21:08:56 +0300

    editing essays

--- js/myComments.js
+++ js/myComments.js
@@ -1,11 +1,52 @@
 app.controller('myCommentsCtrl', function($scope, $http, CommentsFactory) {
     $scope.essays = [];
+    $scope.editingEssay = [];
     CommentsFactory.getComments().then(function(response) {
     	$scope.essays = response.data;
+    	for (var i = 0; i < $scope.essays.length; i++) {
+    		$scope.editingEssay[i] = false;
+    		$scope.essays[i].edited = $scope.essays[i].content;
+    	}
     });
 
+    $scope.editEssay = function(index) {
+    	$scope.editingEssay[index] = true;
+    }
+
+    $scope.saveEssay = function(index) {  	
+    	var edit = false;
+    	if ($scope.essays[index].content == $scope.essays[index].edited) edit = true;
+    	else edit = confirm("Sigur vrei să modifici?");
+		
+		if (edit == true) {
+			$scope.essays[index].content = $scope.essays[index].edited;
+			var id = $scope.essays[index].id;
+			$(function(){
+		        $.ajax({
+		            type: "POST",
+		            url: 'php/edit_essay.php',
+		            data: ({'id': id, 'content': $scope.essays[index].content}),
+		            success: function(data) {}
+		        });
+	    	});
+
+			$scope.editingEssay[index] = false;
+		}
+
+    }
+
+    $scope.cancelEditingEssay = function(index) {    	
+    	var cancel = false;
+    	if ($scope.essays[index].content == $scope.essays[index].edited) cancel = true;
+    	else cancel = confirm("Sigur vrei să renunți la modificări?");
+		
+		if (cancel == true) {
+			$scope.essays[index].edited = $scope.essays[index].content;
+			$scope.editingEssay[index] = false;
+		}
+    }
+
     $scope.deleteEssay = function(index) {
-    	alert(index);
   		var id = $scope.essays[index].id;
     	if (confirm("Sigur vrei să ștergi comentariul?")) {
 			$(function(){
@@ -15,6 +56,7 @@ app.controller('myCommentsCtrl', function($scope, $http, CommentsFactory) {
 		            data: ({'id': id}),
 		            success: function(data) {
 		            	$scope.essays.splice(index, 1);
+		            	$scope.editingEssay.splice(index, 1);
 		            	for (var i = 0; i < $scope.essays.length; i++) {
 		            		$scope.essays[i].index = i;
 		            	}
---
+++ php/edit_essay.php
@@ -0,0 +1,23 @@
+<?php
+	include 'db_connect.php';
+	$id_essay = $_POST['id'];
+	$content = $_POST['content'];
+	session_start();
+	$id = $_SESSION["id"];
+
+	$find_essay_query = "
+		SELECT *
+		FROM essays
+		WHERE id = ". $id_essay ." ;";
+	$find_essay = mysql_query($find_essay_query);
+	$essay_found = mysql_numrows($find_essay);
+
+	if ($essay_found == 1) {
+		if (mysql_result($find_essay, 0, "id_user") == $id) {
+			$edit_query = "	UPDATE essays
+							SET homework = ". $content . "
+							WHERE id = " . $id_essay . ";";;
+			$edit = mysql_query($edit_query);
+		}
+	}
+?>
