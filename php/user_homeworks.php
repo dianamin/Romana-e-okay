2daa888f37eeeb1f44ebda2b95d8e9bba850ec7e
Diana <97.diana@gmail.com>
2015-07-30 03:58:52 +0300

    rating as feedback

--- php/user_homeworks.php
+++ php/user_homeworks.php
@@ -20,13 +20,33 @@
 			/*echo "<table class = 'table table-striped'>";
 			echo "<thead> <tr> <th> # </th> <th> Comentariu </th> </tr> </thead>";
 			echo "<tbody> ";*/
-			for ($i = 0; $i < $essays_count; $i++) {	
+			for ($i = 0; $i < $essays_count; $i++) {
+				$get_ratings = "
+					SELECT *
+					FROM ratings
+					WHERE id_essay = " . mysql_result($essays, $i, "id") . ";";
+				$ratings = mysql_query($get_ratings);
+				$count = mysql_numrows($ratings);
+
+				$total_rating = 0;
+				$average = 0;
+
+				for ($j = 0; $j < $count; $j++) {
+					$total_rating = $total_rating + mysql_result($ratings, $j, "rating");
+				}
+
+
+				if ($count == 0) $average = 0;
+				else $average = $total_rating / $count;
+
 				$aux = array (
 					"index" => $i,
 					"id" => mysql_result($essays, $i, "id"),
 					"content" => mysql_result($essays, $i, "homework"), 
 					"tags" => mysql_result($essays, $i, "tags"),
-					"public" => mysql_result($essays, $i, "public")
+					"public" => mysql_result($essays, $i, "public"),
+					"average" => $average,
+					"ratedBy" => $count
 				);
 				array_push($essays_arr, $aux);
 				//echo "<tr> <td> " . $i . ". </td> <td style = 'text-align: justify;'> " . mysql_result($essays, $i - 1, "homework") . " </td> </tr>";
