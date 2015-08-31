6a94a08aaf6a8531d7108f4a15464b63884271f2
Robert Colca <robery@cncdloga.ro>
2015-08-31 23:58:52 +0300

    Implemented MySQLi connection

--- php/add_score.php
+++ php/add_score.php
@@ -5,24 +5,23 @@
 */
 	include 'db_connect.php';
 
-	$id = $_POST['id'];
-	$score = $_POST['score'];
+	$id = isset($_POST['id']) ? mysql_real_escape_string($_POST['id']) : NULL;
+	$score = isset($_POST['score']) ? mysql_real_escape_string($_POST['score']) : NULL;
 	$find_user_query = "
 		SELECT *
 		FROM users
-		WHERE ID = ". $id ." ;";
+		WHERE id = {$id}";
 
-	$user_result = mysql_query($find_user_query);
-	$user_found = mysql_numrows($user_result);
+	$user_result = $DB->query($find_user_query);
 
-	if ($user_found != 0) {
-		$score = floor($score / 10) + mysql_result($user_result, 0, "score");
+	if ($user_result->num_rows != 0) {
+		//$score = floor($score / 10) + mysql_result($user_result, 0, "score");
 		$update_score_query = "
 			UPDATE users
-			SET score = ". $score . "
-			WHERE id = " . $id . ";";
+			SET score = score + $score
+			WHERE id = {$id}";
 
-		$score_result = mysql_query($update_score_query);
+		$score_result = $DB->query($update_score_query);
 	}
 
 	echo $score;
