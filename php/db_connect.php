56a339f35dab7060d9f07b347f5ef0a482403015
Robert Colca <robery@cncdloga.ro>
2015-08-31 23:40:10 +0300

    Implemented MySQLi connection

--- php/db_connect.php
+++ php/db_connect.php
@@ -2,8 +2,11 @@
 	$host = "";
 	$username = "";
 	$password = "";
-	$db = "Romana-e-okay";
-	$c = mysql_connect($host, $username, $password);
-	@mysql_select_db($db);
+	$dbname = "Romana-e-okay";
+	
+	$DB = new MySQLi($host, $username, $password, $dbname); 
+	
+	//$c = mysql_connect($host, $username, $password);
+	//@mysql_select_db($db);
 	error_reporting(0);
 ?>
