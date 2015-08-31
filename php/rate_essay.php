ef1699979f3e1bb2a0724145ab8ea4783fa30dee
Robert Colca <robery@cncdloga.ro>
2015-09-01 01:44:14 +0300

    Implemented MySQLi connection

--- php/rate_essay.php
+++ php/rate_essay.php
@@ -12,23 +12,23 @@
 	$find_essay_query = "
 		SELECT *
 		FROM ratings
-		WHERE id_essay = ". $id_essay ." 
-		AND id_user = " . $id . ";";
-	$find_essay = mysql_query($find_essay_query);
-	$essay_found = mysql_numrows($find_essay);
+		WHERE id_essay = '{$id_essay}' 
+		AND id_user = '{$id}' ";
+	$find_essay = $DB->query($find_essay_query);
+	$essay_found = $find_essay->num_rows;
 
 	echo $rating;
 
 	if ($essay_found == 1) {
 		$edit_query = "
 			UPDATE ratings
-			SET rating = ". $rating . "
-			WHERE id_essay = " . $id_essay . "
-			AND id_user = " . $id . ";";
-		$edit = mysql_query($edit_query);
+			SET rating = '{$rating}'
+			WHERE id_essay = '{$id_essay}'
+			AND id_user = '{$id}' ";
+		$edit = $DB->query($edit_query);
 	}
 	else {
-		$insert_query = "INSERT INTO ratings (id_user, id_essay, rating) VALUES ('$id', '$id_essay', '$rating')";
-		$inserted = mysql_query($insert_query);
+		$insert_query = "INSERT INTO ratings (id_user, id_essay, rating) VALUES ('{$id}', '{$id_essay}', '{$rating}')";
+		$inserted = $DB->query($insert_query);
 	}
 ?>
