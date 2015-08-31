33a51e6bdf19f8b5645b7db3f5dbe9813c8f9405
Robert Colca <robery@cncdloga.ro>
2015-09-01 01:27:45 +0300

    Implemented MySQLi connection

--- php/get_user_progress.php
+++ php/get_user_progress.php
@@ -4,7 +4,7 @@
 	Used in pageChanging.js
 */
 	include 'db_connect.php';
-	mysql_query("set names 'utf8'");
+	$DB->query("set names 'utf8'");
 	
 	session_start();
 	$id = $_SESSION["id"];
@@ -16,27 +16,28 @@
 		$get_lessons = "
 			SELECT *
 			FROM lessons
-		;";
-		$lessons = mysql_query($get_lessons);
-		$lessons_count = mysql_numrows($lessons);
+		";
+		$lessons = $DB->query($get_lessons);
+		$lessons_count = $lessons->num_rows;
+		$lesson = $lessons->fetch_array(MYSQLI_ASSOC);
 
 		for ($i = 0; $i < $lessons_count; $i++) {
-			$lesson_id = mysql_result($lessons, $i, "global_id");
-			$lesson_name = mysql_result($lessons, $i, "name");
+			$lesson_id = $lesson[$i]['global_id'];
+			$lesson_name = $lesson[$i]['name'];
 			$read_query = "
 				SELECT *
 				FROM progress
-				WHERE id_user = " . $id . "
-				AND id_lesson = " . $lesson_id . "
-			;";
-			$read_result = mysql_query($read_query);
-			$read = mysql_numrows($read_result);
+				WHERE id_user =  '{$id}'
+				AND id_lesson = '{$lesson_id}' 
+			";
+			$read_result = $DB->query($read_query);
+			$read = $read_result->num_rows;
 			$aux = array(
 				"name" => $lesson_name,
 				"id" => $lesson_id,
 				"read" => $read,
 				"index" => $i,
-				"type" => mysql_result($lessons, $i, "type")
+				"type" => $lesson[$i]['type']
 			);
 			array_push($lessons_arr, $aux);
 		}
