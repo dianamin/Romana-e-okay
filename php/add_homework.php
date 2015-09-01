a1abda1d1df07a5e70c0c8e805ed240b02966f69
Robert Colca <robery@cncdloga.ro>
2015-09-01 22:20:32 +0300

    fixed security issue
    Fixed security issue and moved the session_start function

--- php/add_homework.php
+++ php/add_homework.php
@@ -8,11 +8,10 @@
 	
 	$s = isset($_POST['s']) ? $DB->real_escape_string($_POST['s']) : NULL;
 	$t = isset($_POST['tags']) ? $DB->real_escape_string($_POST['tags']) : NULL;
-	
-	session_start();
+
 	$id = $_SESSION["id"];
 	
-	$insert_text = "INSERT INTO essays(id_user, id, homework, tags, public, reported) VALUES (" . $id . ", 'NULL', '". $s . "', '" . $t ."', 'false', 0)";
+	$insert_text = "INSERT INTO essays(id_user, id, homework, tags, public, reported) VALUES ('{$id}' , 'NULL', '{$s}', '{$t}', 'false', 0)";
 	
 	//echo $insert_text; why ??
 	
