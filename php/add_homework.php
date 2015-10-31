984da00c342796674dda6ee38027fb60e7d04f9a
Robert Colca <robery@cncdloga.ro>
2015-10-31 22:52:40 +0200

    Making sure id it's declared

--- php/add_homework.php
+++ php/add_homework.php
@@ -9,7 +9,7 @@
 	$s = isset($_POST['s']) ? $DB->real_escape_string($_POST['s']) : NULL;
 	$t = isset($_POST['tags']) ? $DB->real_escape_string($_POST['tags']) : NULL;
 
-	$id = $_SESSION["id"];
+	$id = isset($_SESSION["id"]) ? $_SESSION['id'] : NULL;
 	
 	$insert_text = "INSERT INTO essays(id_user, id, homework, tags, public, reported) VALUES ('{$id}' , 'NULL', '{$s}', '{$t}', 'false', 0)";
 	
